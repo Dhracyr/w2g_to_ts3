75fd9b93241ea5ff71c2f94b162ddf455e0dc935
Dhracyr <moritz@goemmel.com>
2024-06-26 22:55:38 +0200

    Add json support and functionality for opener

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+import json
 import telnetlib
+import webbrowser
 
 import requests
 import pyperclip
 import pyautogui
 import time
 
-from APIKeys_and_URLs import apiKey
+
+# Load configuration from JSON file
+with open('config.json', 'r') as config_file:
+    config = json.load(config_file)
+
+w2g_apiKey = config.get('w2g_apiKey', '')
+teamspeak_apiKey = config.get('teamspeak_apiKey', '')
+dnd_beyond_url = config.get('dnd_beyond_url', '')
+foundry_url = config.get('foundry_url', '')
 
 
 # Step 1: Create a Watch2Gether lobby
@@ -17,7 +27,7 @@ def create_w2g_lobby():
         'Content-Type': 'application/json'
     }
     data = {
-        "w2g_api_key": "nkmz57lc6o5kz1k9eipzx1tjq3vti5wzbdbl4096klp5edo1y86t0k5babnhdp2y",
+        "w2g_api_key": w2g_apiKey,
         "share": "https://www.youtube.com/watch?v=6Dh-RL__uN4",
         "bg_color": "#3a4759",
         "bg_opacity": "50"
@@ -36,11 +46,11 @@ def paste_into_teamspeak():
 
 
 # Function to connect to Teamspeak and send a message
-def send_to_teamspeak(message, server='localhost', port=25639, apiKey=apiKey, target_mode=2, target=1):
+def send_to_teamspeak(message, server='localhost', port=25639, teamspeak_apiKey=teamspeak_apiKey, target_mode=2, target=1):
     try:
         tn = telnetlib.Telnet(server, port)
         tn.read_until(b"TS3 Client")
-        tn.write(f"auth apikey={apiKey}\n".encode('utf-8'))
+        tn.write(f"auth apikey={teamspeak_apiKey}\n".encode('utf-8'))
         tn.read_until(b"msg=ok")
 
         tn.write(f"sendtextmessage targetmode={target_mode} target={target} msg={message}\n".encode('utf-8'))
@@ -51,12 +61,34 @@ def send_to_teamspeak(message, server='localhost', port=25639, apiKey=apiKey, ta
         print(f"An error occurred while connecting to Teamspeak: {e}")
 
 
+def open_dnd_beyond(dnd_beyond_url):
+    webbrowser.open(dnd_beyond_url)
+
+
+def open_foundry(foundry_url):
+    webbrowser.open(foundry_url)
+
+
 def main():
-    invite_link = create_w2g_lobby()
-    pyperclip.copy(invite_link)
-    time.sleep(5)  # Give some time to switch to Teamspeak chat window
-    send_to_teamspeak(invite_link)
-    open_dndbeyond(dndbeyond_url)
+    if w2g_apiKey == '':
+        print("No Watch2Gether API Key was set. Skipping...")
+    else:
+        invite_link = create_w2g_lobby()
+        pyperclip.copy(invite_link)
+        if teamspeak_apiKey == '':
+            print("No Teamspeak API Key was set. The link will be pasted in 5 seconds...")
+            time.sleep(5)
+            paste_into_teamspeak()
+        else:
+            send_to_teamspeak(invite_link)
+    if dnd_beyond_url == '':
+        print("No DnD-Beyond Character links was set. Skipping...")
+    else:
+        open_dnd_beyond(dnd_beyond_url)
+    if foundry_url == '':
+        print("No DnD-Lobby was set. Skipping...")
+    else:
+        open_foundry(foundry_url)
 
 
 if __name__ == "__main__":
